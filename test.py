353f960501111e3c9cd9ae2078eb987c4b9e6937
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2024-01-25 22:16:34 +0900

    [WIP] fix ...

--- test.py
+++ test.py
@@ -10,13 +10,32 @@ di = DrawInterface()
 ras = ra.RobotAssemblerSettings()
 ras.parseYaml(proj_dir + '/irsl_assembler_config.yaml')
 ras.insertPartsFromYaml('sample.yaml')
+rasu=ra.RoboasmUtil(ras)
 
 f=ra.RoboasmFile('gripper.roboasm')
-rasu=ra.RoboasmUtil(ras)
 rb=rasu.makeRobot(f)
 
 cr=ra.RoboasmBodyCreator(proj_dir)
 
-bd=cr.createBody(rb)
+bd=cr.createBodyRaw(rb)
 
 iu.exportBody('/tmp/hoge.body', bd)
+
+# ---
+
+rau=ra.RoboasmUtil(proj_dir + '/irsl_assembler_config.yaml')
+rau.settings.insertPartsFromYaml('sample.yaml')
+#f=ra.RoboasmFile('gripper.roboasm')
+#rb=rasu.makeRobot(f)
+
+raf=ra.cnoidRAFile('gripper.roboasm')
+rb=raf.makeRobot(rau)
+
+cr=ra.RoboasmBodyCreator(proj_dir)
+bd=cr.createBody(rb, raf)
+
+'/home/irsl/temp/make_parts/fixed_finger_mm.stl'
+'/home/irsl/temp/make_parts/move_plate_with_hole.stl'
+
+aa=mkshapes.loadMesh('/home/irsl/temp/make_parts/move_plate_with_hole.stl', scale=0.001)
+bb=mkshapes.loadMesh('/home/irsl/temp/make_parts/fixed_finger_mm.stl', scale=0.001)
